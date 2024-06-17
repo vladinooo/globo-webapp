b70077153f58d8f4a175d0c6f539eb858fcaad6e
Vlad Hartmann <vladimir.hartmann@kbc.com>
2024-06-17 15:04:06 +0200

    Added globo_web_app

---
+++ globo_web_app/variables.tf
@@ -0,0 +1,63 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
+  sensitive   = true
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region to use for resources"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Base CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "Enable DNS hostnames in VPC"
+  default     = true
+}
+
+variable "vpc_public_subnet_cidr_block" {
+  type        = list(string)
+  description = "CIDR Block for Public Subnets in VPC"
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  description = "Map a public IP address for Subnet instances"
+  default     = true
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Type for EC2 Instance"
+  default     = "t2.micro"
+}
+
+variable "company" {
+  type        = string
+  description = "Company name for resource tagging"
+  default     = "Globomantics"
+}
+
+variable "project" {
+  type        = string
+  description = "Project name for resource tagging"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "Billing code for resource tagging"
+}
