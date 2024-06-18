ea6a6e2db91e9bcda49c73ad7b398be2c3cd08b0
Vlad Hartmann <vladimir.hartmann@kbc.com>
2024-06-18 14:57:28 +0200

    Added Random provider, IAM roles, tidy up

--- globo_web_app/variables.tf
+++ globo_web_app/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  type        = string
-  description = "AWS access key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS secret key"
-  sensitive   = true
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region to use for resources"
